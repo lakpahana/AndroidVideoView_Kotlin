f8cba901ca68401fdd5f91b385128423d8134956
lakpahana <agslakpahana@gmail.com>
2022-08-12 22:58:14 +0530

    fnal

---
+++ app/src/main/java/com/example/videoviewdemo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.videoviewdemo
+
+import android.net.Uri
+import android.net.Uri.parse
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import java.net.URI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //mediaController.show(0)
+        val uri: Uri = parse("android.resource://"+packageName+"/"+R.raw.vlog)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
